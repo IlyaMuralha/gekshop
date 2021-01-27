dbed16f447c15de8bfeee45b2a55104c39e7972a
“IlyaMuralha” <muralhakuzmenko@gmail.com>
2021-01-27 14:47:09 +0500

    Deleted context.html

--- mainapp/views.py
+++ mainapp/views.py
@@ -29,19 +29,3 @@ def products(request):
     return render(request, 'mainapp/products.html', context)
 
 
-def test_context(request):
-    context = {
-        'title': 'test Context',
-        'header': 'Добро пожаловать!',
-        'username': 'Илья Кузьменко',
-        'products': [
-            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': '6 090,00'},
-            {'name': 'Синяя куртка The North Face', 'price': '23 725,00'},
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': '3 390,00'},
-        ],
-        'promotion': True,
-        'products_of_promotion': [
-            {'name': 'Черный рюкзак Nike Heritage', 'price': '2 340,00'},
-        ]
-    }
-    return render(request, 'mainapp/context.html', context)
