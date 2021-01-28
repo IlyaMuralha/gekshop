91bf81c0bbe3cf758cfc78cacafcd721b14139fe
“IlyaMuralha” <muralhakuzmenko@gmail.com>
2021-01-28 11:43:31 +0500

    home_work_3 done

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import include
 
 from mainapp import views as mainapp_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mainapp_views.index, name='index'),
-    path('products/', mainapp_views.products, name='products'),
+    path('products/', include('mainapp.urls', namespace='products')),
 ]
 
 
