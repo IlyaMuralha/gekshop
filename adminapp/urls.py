4ad809c1817800e8b2d58eb376cb5b5666c28bf3
“IlyaMuralha” <muralhakuzmenko@gmail.com>
2021-02-15 16:39:41 +0500

    added delete controler

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('admin-users-read/', adminapp.admin_users_read, name='admin_users_read'),
     path('admin-users-create/', adminapp.admin_users_create, name='admin_users_create'),
     path('admin-users-update/<int:id>/', adminapp.admin_users_update, name='admin_users_update'),
+    path('admin-users-delete/<int:id>/', adminapp.admin_users_delete, name='admin_users_delete'),
 ]
