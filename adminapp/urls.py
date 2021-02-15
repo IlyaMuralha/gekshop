75fda75218473084329ef8e4dbf89167429b3409
“IlyaMuralha” <muralhakuzmenko@gmail.com>
2021-02-15 07:39:39 +0500

    created admin_user_create

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -8,4 +8,5 @@ app_name = 'adminapp'
 urlpatterns = [
     path('', adminapp.index, name='index'),
     path('admin-users-read/', adminapp.admin_users_read, name='admin_users_read'),
+    path('admin-users-create/', adminapp.admin_users_create, name='admin_users_create'),
 ]
