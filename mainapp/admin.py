fc9253ea3a83d4f46646673272af5cd98442a0af
“IlyaMuralha” <muralhakuzmenko@gmail.com>
2021-02-17 16:40:32 +0500

    added class ProductAdmin

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -3,5 +3,13 @@ from django.contrib import admin
 from mainapp.models import ProductCategory, Product
 
 admin.site.register(ProductCategory)
-admin.site.register(Product)
 
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'short_description', 'price', 'quantity')
+    fields = ('name', 'image', 'description', 'short_description', ('price', 'quantity'), 'category')
+    # # Запрет на редактирование
+    # readonly_fields = ('category', 'short_description')
+    ordering = ('name', 'price', 'category')
+    search_fields = ('name', 'category__name')
